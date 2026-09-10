dd9de3b1c119843d7a6acf2e90d5431a9d1445a1
Rajveer kumar <kumarrajveer0155@gmail.com>
2026-09-10 23:13:56 +0530

    feat: add events urls, views, and forms

---
+++ events/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.event_list, name='event_list'),
+    path('event/<int:pk>/', views.event_detail, name='event_detail'),
+    path('event/new/', views.event_create, name='event_create'),
+    path('event/<int:pk>/rsvp/', views.toggle_rsvp, name='toggle_rsvp'),
+]
