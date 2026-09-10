35d02b88c28a8e595f71a8e720b8e06d705b3646
Rajveer kumar <kumarrajveer0155@gmail.com>
2026-09-10 21:21:45 +0530

    fix: configure allowed hosts and csrf origins

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-5vdb_j3se6=mc=mg)fh8)(!&lr2v+f4+neds!6rk^gch3j_y*0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', '.onrender.com', 'localhost', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.onrender.com',
+]
 
 
 # Application definition
