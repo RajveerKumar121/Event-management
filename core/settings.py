3fbdc3a6de5fe0cdf233a4ac2fb821870646f2a4
Rajveer kumar <kumarrajveer0155@gmail.com>
2026-09-10 22:10:45 +0530

    feat: register models in admin and fix static storage

--- core/settings.py
+++ core/settings.py
@@ -35,12 +35,14 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'events',  
 ]
 
 MIDDLEWARE = [
