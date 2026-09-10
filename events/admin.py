3b7b2e71e4faa596597114009377afc507ef16a1
Rajveer kumar <kumarrajveer0155@gmail.com>
2026-09-10 22:04:27 +0530

    feat: register Event models in admin panel

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Event, RSVP
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'location', 'start_time', 'capacity', 'organizer')
+    list_filter = ('category', 'start_time')
+    search_fields = ('title', 'description', 'location')
+
+@admin.register(RSVP)
+class RSVPAdmin(admin.ModelAdmin):
+    list_display = ('event', 'user', 'registered_at')
+    list_filter = ('event', 'registered_at')
